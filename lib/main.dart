884814607956188fbeeaa976cd417861c34e9d6d
Adam Ashraf Hamdy <adamashrafhamdy11@gmail>
2026-04-14 08:45:23 +0200

    add app icon

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(FruitHub());
+}
+
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
