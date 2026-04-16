feb2ae339c406d8401fe78e83a50f37c900d2a09
Adam Ashraf Hamdy <adamashrafhamdy11@gmail>
2026-04-16 19:23:45 +0200

    add  custom text form field

---
+++ lib/core/widgets/custom_text_form_feild.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormFeild extends StatelessWidget {
+  const CustomTextFormFeild({
+    super.key,
+    required this.hintText,
+    this.sufIcon,
+    this.keyboardType,
+  });
+  final String hintText;
+  final Widget? sufIcon;
+  final TextInputType? keyboardType;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: keyboardType,
+      onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
+      decoration: InputDecoration(
+        hintText: hintText,
+        suffixIcon: sufIcon,
+        fillColor: Color(0XFFf9fbfa),
+        filled: true,
+        enabledBorder: outBorder(),
+        focusedBorder: outBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder outBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0xffe5e9ea), width: 1),
+      borderRadius: BorderRadius.circular(12),
+    );
+  }
+}
--- lib/feature/auth/presentation/view/login_view_body.dart
+++ lib/feature/auth/presentation/view/login_view_body.dart
@@ -1,13 +1,36 @@
-
+import 'package:e_commerce_app/constants.dart';
 import 'package:e_commerce_app/core/widgets/custom_app_bar.dart';
+import 'package:e_commerce_app/core/widgets/custom_text_form_feild.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: customAppBar(context, title: 'تسجيل الدخول'));
+    return Scaffold(
+      appBar: customAppBar(context, title: 'تسجيل الدخول'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: hlPadding),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Gap(24),
+              CustomTextFormFeild(
+                hintText: 'البريد الالكتروني',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              Gap(16),
+              CustomTextFormFeild(
+                hintText: 'كلمة المرور',
+                keyboardType: TextInputType.visiblePassword,
+                sufIcon: Icon(Icons.remove_red_eye_outlined),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-
 }
